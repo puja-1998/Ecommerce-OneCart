2d2f4b5f75a4f000e488cf909855328d0a459476
Puja-1998 <ghodkepooja98@gmail.com>
2025-08-27 17:35:07 +0530

    updated

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -126,7 +126,7 @@ export const adminLogin = async (req, res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite:"none",
+            sameSite:"None",
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json({
